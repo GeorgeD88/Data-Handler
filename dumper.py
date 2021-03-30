abec8f106a2b82dbbd88d55908127d422f783bad
George Doujaiji <GeorgeD.88888@gmail.com>
2021-03-30 16:55:23 -0400

    Restructure chunk pack use lists instead of dicts
    When I first made the data handler, it split data into things I called chunk packs, which were lists containing lists of data.
    This conflicted with loading data from json which ABSOLUTELY needed the keys, so instead I switched everything to use dicts.
    This works much much better and allows me to join CSV and JSON data easily and dump data more efficiently.

--- dumper.py
+++ dumper.py
@@ -8,36 +8,53 @@ module_name = __file__.split("/")[-1]  # gets name of the module (filename) from
 logger = setup_logger(module_name, 'datahandler_activity.log')
 
 
-def dump_to_json(chunk_pack: list, export_name: str, indent=2):
+def dump_to_json(chunk_pack: dict, folder_name: str = None, indent: int or None = 2):
     """
-    Dumps chunk pack (list of lists of dicts) into a json file
+    Dumps chunk pack (dict of items) into a json file
+
+    If you would like the files to be stored in a folder,
+        use the keyword argument folder_name with the desired folder name, e.g.
+
+    dumper.dump_to_json(chunk_pack, "folder_name")
+
+    If you want your json files to be indented at each line with an indent other than 2,
+        use the keyword argument indent with the preferred indent or None for no indent, e.g.
+
+    dumper.dump_to_json(chunk_pack, indent=4)
 
     Args:
-        chunk_pack (list): The data to be dumped into json in the form of a chunk pack (list of lists)
-        export_name (str): The filename of the json file we're dumping to.
-        indent (int or None, optional): The indent of each json file being created, defaults to 2,
-            but if an indent of None is given, the files won't be indented.
+        chunk_pack (dict): The data to be dumped into json in the form of a chunk pack (dict of items)
+        folder_name (str, optional): The name of the folder we're dumping to,
+            if left empty will default to None and not store the files in a folder.
+        indent (int or None, optional): The indent of each json file being created, defaults to 2, but can be
+            overwritten if a different indent is given. if an indent of None is given, the files won't be indented.
     """
 
     start = time()
     pack_size = len(chunk_pack)
+    pack_counter = 1
 
-    # checks if there are multiple chunks and makes a folder to store them if true
-    if pack_size > 1:
-        os.makedirs(export_name)
-        os.chdir(export_name)
+    # if a folder name is provided (not None) it switches to that directory after making the folder first if needed
+    if folder_name:
+        if not os.path.isdir(folder_name):
+            os.makedirs(folder_name)
+        os.chdir(folder_name)
     logger.info(f'started dumping {pack_size} '
                 f'chunk{"s" if pack_size > 1 else ""} into json')
 
     # goes through every chunk in the chunk pack and dumps each one into its own json file
-    for i in range(pack_size):
-        constructed_filename = f'{export_name}_({i+1},{pack_size}).json'
-        logger.info(f'dumping chunk {i+1}/{pack_size}')
-        with open(constructed_filename, 'w+') as out_json:
-            json.dump({"chunk": chunk_pack[i]}, out_json, indent=indent)
-        logger.info(f'dumped chunk {i+1}/{pack_size}')
+    for chunk_key, chunk_value in chunk_pack.items():
+        logger.info(f'dumping chunk {chunk_key} {pack_counter}/{pack_size}')
+        export_file = chunk_key + '.json'
 
-    # if new folder was made for dumping process, it will switch back to the parent directory
+        # after creating the data_dump variable according to the conditions, it finally dumps it into the json file
+        with open(export_file, 'w+') as out_json:
+            json.dump({chunk_key: chunk_value}, out_json, indent=indent)
+        logger.info(f'dumped chunk {chunk_key} {pack_counter}/{pack_size}')
+
+        pack_counter += 1
+
+    # if new folder was made for the dumping process, it will switch back to the parent directory
     if pack_size > 1:
         os.chdir('..')
     runtime = time() - start
--- loader.py
+++ loader.py
@@ -8,9 +8,9 @@ module_name = __file__.split("/")[-1]  # gets name of the module (filename) from
 logger = setup_logger(module_name, 'datahandler_activity.log')
 
 
-def load_from_csv(csv_filename: str, chunk_size=1000000) -> list:
+def load_from_csv(csv_filename: str, chunk_size=1000000) -> dict:
     """
-    Loads data from file of filetype .csv and returns the data as a pack (list) of chunks (list of dicts)
+    Loads data from file of filetype .csv and returns the data as a pack (dict) of chunks (list of dicts)
 
     If a different chunk size is preferred, use the keyword argument chunk_size with the desired size, e.g.
 
@@ -19,24 +19,27 @@ def load_from_csv(csv_filename: str, chunk_size=1000000) -> list:
     Args:
         csv_filename (str): The filename of the csv file we're loading in.
         chunk_size (int or None, optional): The size of each chunk, defaults to 1000000 rows,
-            but if a size of None is given, the chunk will not have a limited amount of rows.
+            but if a size of None is given, the chunk will not limit the number of rows.
 
     Returns:
-        Returns a pack of chunks (list of lists) and each chunk (list of dictionaries),
+        Returns a pack of chunks (dict of elements) and each chunk (list),
         contains many rows (dictionaries), each row from each csv row::
-            [
-                [
-                    {'row1': row1},
-                    {'row2': row2}
-                ],
-                [
-                    {'row3': row3},
-                    {'row4': row4},
-                    {...}
+            {
+                "csv_file_chunk1":
+                    [
+                        {'row1': row1},
+                        {'row2': row2},
+                        {...}
+                    ],
+                "csv_file_chunk2":
+                    [
+                        {'row3': row3},
+                        {'row4': row4},
+                        {...}
 
-                ],
-                [...]
-            ]
+                    ],
+                    [...]
+            }
     """
 
     start = time()
@@ -45,25 +48,27 @@ def load_from_csv(csv_filename: str, chunk_size=1000000) -> list:
         logger.info(f'started reading csv file {csv_filename}')
         header = next(csv_reader)
         parts = 1
-        chunk_pack = []  # the chunk pack is a list that will store each chunk of data
+        chunk_pack = {}  # the chunk pack is a dict that will store each chunk of data
         chunk_insert = []  # the chunk insert is where the chunk of data is constructed to be inserted into the pack
 
-        # if a chunk size of None was given, it'll keep the data in one chunk instead of splitting it into many
+        # if a chunk size of None was given, it'll keep the data in one chunk instead of splitting it into many chunks
         if not chunk_size:
             for row in csv_reader:
                 row_insert = {}
                 for key_i in range(len(header)):
                     row_insert[header[key_i]] = row[key_i]
                 chunk_insert.append(row_insert)
-            chunk_pack.append(chunk_insert.copy())
-        # else if a chunk size IS give, it'll split the data into chunks using the given chunk size as a limit
+            key_name = csv_filename[:-4] + '_chunk1'
+            chunk_pack[key_name] = chunk_insert.copy()
+        # else if a chunk size IS given, it'll split the data into chunks using the given chunk size as a limit
         else:
             counter = 0
             for row in csv_reader:
                 # if the row counter reaches the same number as the chunk size limit, it'll start adding to a new chunk
                 if counter == chunk_size:
-                    logger.info(f'sealed chunk {len(chunk_pack)}')
-                    chunk_pack.append(chunk_insert.copy())
+                    logger.info(f'sealed chunk {parts}')
+                    key_name = csv_filename[:-4] + '_chunk' + str(parts)
+                    chunk_pack[key_name] = chunk_insert.copy()
                     chunk_insert = []
                     counter = 0
                     parts += 1
@@ -74,31 +79,25 @@ def load_from_csv(csv_filename: str, chunk_size=1000000) -> list:
                 counter += 1
             # the last chunk most likely won't hit the chunk size limit, so at the end we'll just add it manually
             if chunk_insert:
-                chunk_pack.append(chunk_insert.copy())
+                key_name = csv_filename[:-4] + '_chunk' + str(parts)
+                chunk_pack[key_name] = chunk_insert.copy()
     runtime = time() - start
     logger.info(f'extracted csv data from file {csv_filename} into {parts} '
                 f'chunk{"s" if parts > 2 else ""} in {round(runtime, 2)} sec')
     return chunk_pack
 
 
-def load_from_json(json_filename: str, simple_json: bool = False):
+def load_from_json(json_filename: str) -> dict:
     """
-    Loads data from file of filetype .json and returns the data as a chunk (dict),
-        with loading from json instead of csv, the chunk is just the same dict as the json file itself
-        instead of the chunk being a list of dicts, each dict being a row from the csv.
-
-    To remove redundant outer json shell because all the data is in one key,
-        use the keyword argument simple_json with True value, e.g.
-
-    loader.load_from_json("json_file.json", simple_json=True)
+    Loads data from file of filetype .json and returns the data as a chunk pack (dict) with the items from,
+        the file with loading from json instead of csv, the chunk is just the same dict as the json file
+        itself instead of the chunk being a list of dicts, each dict being a row from the csv.
 
     Args:
         json_filename (str): The filename of the csv file we're loading in.
-        simple_json (bool, optional): The simple status of the file defaults to False,
-            but if status of True is given, the redundant outer json shell will be removed.
 
     Returns:
-        Returns the data (dict), nothing fancy or extra::
+        Returns the same data (dict) as the file, nothing fancy or extra::
             {
                 "key": value,
                 "key": value,
@@ -111,10 +110,6 @@ def load_from_json(json_filename: str, simple_json: bool = False):
     with open(json_filename) as in_json:
         data = json.load(in_json)
         logger.info(f'started reading json file {json_filename}')
-        # if all of the data is in just one json object, it will only return that nested value
-        if simple_json:
-            only_key = list(data.keys())[0]
-            data = data[only_key]
     runtime = time() - start
     logger.info(f'extracted json data from file {json_filename} in {round(runtime, 2)} sec')
     return data
